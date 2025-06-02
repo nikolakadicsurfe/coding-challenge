3423167a3c079e81f092b1123975deda5a6b8ac3
Nikola Kadić <kadicnikola3@gmail.com>
2025-06-02 22:21:15 +0200

    Start tests for fetching user

--- surfe_backend/api/tests.py
+++ surfe_backend/api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from api.models import User, Action
 
-# Create your tests here.
+class UserAPITests(TestCase):
+    def setUp(self):
+        # Create sample users
+        User.objects.create(id=1, name='John Doe', created_at='2022-04-14T11:12:22.758Z')
+        User.objects.create(id=2, name='Jane Smith', created_at='2022-05-14T11:12:22.758Z')
+
+    def test_fetch_user_by_id(self):
+        response = self.client.get(reverse('fetch_user', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'id': 1,
+            'name': 'John Doe',
+            'created_at': '2022-04-14T11:12:22.758Z'
+        })
+
+    # Additional tests for other endpoints will be added here
