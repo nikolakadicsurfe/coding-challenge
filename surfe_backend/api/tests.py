71ecb807b1b875403d2fc3e891a8eac97958a7c3
Nikola Kadić <kadicnikola3@gmail.com>
2025-06-02 22:23:20 +0200

    Start tests for action API

--- surfe_backend/api/tests.py
+++ surfe_backend/api/tests.py
@@ -19,3 +19,21 @@ class UserAPITests(TestCase):
         })
 
     # Additional tests for other endpoints will be added here
+
+class ActionAPITests(TestCase):
+    def setUp(self):
+        # Create sample users
+        user1 = User.objects.create(id=1, name='John Doe', created_at=parse_datetime('2022-04-14T11:12:22.758Z'))
+        user2 = User.objects.create(id=2, name='Jane Smith', created_at=parse_datetime('2022-05-14T11:12:22.758Z'))
+
+        # Create sample actions
+        Action.objects.create(id=1, type='LOGIN', user_id=user1, created_at=parse_datetime('2022-04-14T12:00:00.000Z'))
+        Action.objects.create(id=2, type='LOGOUT', user_id=user1, created_at=parse_datetime('2022-04-14T13:00:00.000Z'))
+        Action.objects.create(id=3, type='LOGIN', user_id=user2, created_at=parse_datetime('2022-05-14T12:00:00.000Z'))
+
+    def test_get_total_actions_for_user(self):
+        response = self.client.get(reverse('get_total_actions', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'count': 2
+        })
